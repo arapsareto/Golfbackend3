44833f08471168a24da071162c80ed28428deff8
martinsareto <martin.kiprotich@student.moringaschool.com>
2022-09-16 16:40:36 +0300

    spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ Course.create(location: "Kenya",name: "kericho Golf course")
 Course.create(location: "Kenya",name: "railways golf course")
 Course.create(location: "Kenya",name: "Muthaiga golf course")
 
-
  Player.create(name: "Martin Sareto",phone: 0712345643)
  Player.create(name: "Korir Allan",phone: 0712345643)
  Player.create(name: "Barsulai",phone: 0712345643)
