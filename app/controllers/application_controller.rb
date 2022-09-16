bb02cce1f007553554b470ae7f5eaac361f78c44
arapsareto <martinsaretto@gmail.com>
2022-09-16 15:28:44 +0300

    adding my backend controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
     
     # Add your routes here
+    get "/" do
+      %{
+        backend repository
+      }
+    end
     get "/courses" do
      data = Course.all
      data.to_json
