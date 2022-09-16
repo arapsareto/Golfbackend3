9e861811cc47b185dfa99164b22bbc16435d0531
arapsareto <martinsaretto@gmail.com>
2022-09-16 15:37:22 +0300

    added tournaments table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@ Course.create(location: "Kenya",name: "railways golf course")
 Course.create(location: "Kenya",name: "Muthaiga golf course")
 
 
-# Player.create(name: "Martin Sareto",phone: 0712345643)
-# Player.create(name: "Korir Allan",phone: 0712345643)
-# Player.create(name: "Barsulai",phone: 0712345643)
-# Player.create(name: "Kombi sagasi",phone: 0712345643)
+ Player.create(name: "Martin Sareto",phone: 0712345643)
+ Player.create(name: "Korir Allan",phone: 0712345643)
+ Player.create(name: "Barsulai",phone: 0712345643)
+ Player.create(name: "Kombi sagasi",phone: 0712345643)
+
+ Tournament.create(location: Faker::Address.city ,name: "Masters" )
+ Tournament.create(location: Faker::Address.city ,name: "US PGA" )
+ Tournament.create(location: Faker::Address.city,name: "Us Open")
+ Tournament.create(location: Faker::Address.city,name: "The Open")
